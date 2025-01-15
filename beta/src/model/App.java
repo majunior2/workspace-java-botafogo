accd03539708a16c25677225c5911d1f6bcf3eb5
MAURO DE MELLO JUNIOR <mauro.junior@aluno.senac.rio>
2025-01-14 21:43:07 -0300

    exercicio java

---
+++ beta/src/model/App.java
@@ -0,0 +1,17 @@
+package model;
+
+public class App {
+
+	public static void main(String[] args) {
+		Tarefa exemplo = new Tarefa();
+		
+		exemplo.setId(1);
+		exemplo.setDescricao("Comprar Batata");
+		exemplo.setPrazo(1);
+		exemplo.setFinalizada(false); 
+		
+		System.out.println(exemplo.getDescricao());
+
+	}
+
+}
